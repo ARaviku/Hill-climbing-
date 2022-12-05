a29871dd35eb2e9334f24dd2543a4414af68fcd5
Anurekha Ravikumar <100729318+ARaviku@users.noreply.github.com>
2022-12-05 11:45:06 -0500

    Update run_climber.cpp

--- run_climber.cpp
+++ run_climber.cpp
@@ -1,19 +1,8 @@
 #include <string>
-// --- Your code here
 #include"climber.h"
-
-
-
-// ---
 
 int main(int argc, char *argv[])
 {
-    // NOTE: these filenames are relative to the current working directory.
-    // if you get a "file not found" error, then you (or vscode) are not running the executable from
-    // the right directory. Check that your "settings.json" file in the ".vscode" folder contains:
-    //   "cmake.debugConfig": {
-    //    "cwd": "${workspaceFolder}"
-    //   }
     std::string infilename = "input.txt";
     std::string outfilename = "output.txt";
     if (argc == 2) { 
